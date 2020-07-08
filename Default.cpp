3f152e477a57759faf8b94c35a37b3370ae5c1a4
Seugnwon Lee <65075764+lsw6684@users.noreply.github.com>
2020-07-08 19:30:50 +0900

    Update Default.cpp

--- Default.cpp
+++ Default.cpp
@@ -7,9 +7,8 @@ int Adder(int num1=1, int num2=2)
 
 int main(void)
 {
-	std::cout<<Adder()<<std::endl;
-	std::cout<<Adder(5)<<std::endl;
-	std::cout<<Adder(3,5)<<std::endl;
+	std::cout<<Adder()<<std::endl;		//그대로 1과 2
+	std::cout<<Adder(5)<<std::endl;		//인자를 하나만 전달, 첫 번째 매개변수로 전달
+	std::cout<<Adder(3,5)<<std::endl;	//두 개의 인자를 직접 전달, 이러한 경우 매개변수의 디폴트 값은 의미가 없다.
 	return 0;
-	
 }
