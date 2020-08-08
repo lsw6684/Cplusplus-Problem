cb3142cd3ab96429d7c35528e3b0f4ce52872d6c
Seugnwon Lee <65075764+lsw6684@users.noreply.github.com>
2020-08-08 20:38:59 +0900

    Rename 2차원 배열 선언 및 활용.cpp to Practices/2차원 배열 선언 및 활용.cpp

--- Practices/2차원 배열 선언 및 활용.cpp
+++ Practices/2차원 배열 선언 및 활용.cpp
@@ -39,7 +39,7 @@ int main()
 	for(int i = 0; i < 2; i++)
 		for(int j = 0; j < 3; j++)
 		{
-			cout << "Circle [" << i << ", " << j << "]ÀÇ ¸éÀûÀº ";
+			cout << "Circle [" << i << ", " << j << "]Ã€Ã‡ Â¸Ã©Ã€Ã»Ã€Âº ";
 			cout << circles[i][j].getArea() << endl; 
 				}		
 }
