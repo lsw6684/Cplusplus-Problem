6567b19749f5d5c3fe2691a338bc794b1b6ac60d
Seugnwon Lee <lsw6684@hanmail.net>
2020-09-26 22:19:57 +0900

    There are problems about Identifier

--- Practices/dynamic array.cpp
+++ Practices/dynamic array.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 int main()
 {
 	std::cout << "Enter list size : ";
@@ -18,4 +17,45 @@ int main()
 	print(A, n);
 	delete[] A;
 	return 0;
+}
+void initialize(int list[], int size, int value) {
+	for (int i = 0; i < size; i++)
+		list[i] = value;
+}
+void print(int list[], int size) {
+	std::cout << "[ ";
+	for (int i = 0; i < size; i++)
+		std::cout << list[i] << " ";
+	std::cout << "]" << std::endl;
+}
+int* addElement(int list[], int& size, int value) {
+	int* newList = new int[size + 1];
+	if (newList == 0) {
+		std::cout << "Memory allocation error for addElement!" << std::endl;
+		exit(-1);
+	}
+	for (int i = 0; i < size; i++)
+		newList[i] = list[i];
+	if (size)
+		delete[] list;
+	newList[size] = value;
+	size++;
+	return newList;
+}
+int* deleteFirst(int list[], int& size) {
+	if (size <= 1) {
+		if (size) delete list;
+		size = 0;
+		return NULL;
+	}
+	int* newList = new int[size - 1];
+	if (newList == 0) {
+		std::cout << "Memory allocation error for delete First!" << std::endl;
+		exit(-1);
+	}
+	for (int i = 0; i < size - 1; i++)
+		newList[i] = list[i + 1];
+	delete[] list;
+	size--;
+	return newList;
 }
