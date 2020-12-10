c856bcf5a28fea96d8d2a842556283b7986e8d3e
lsw6684 <lsw6684@hanmail.net>
2020-12-10 09:32:15 +0900

    Destructor flow

---
+++ Practices/DestructorFlow.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+private:
+	int baseNum;
+public:
+	Base(int n) : baseNum(n)
+	{
+		cout << "Base() : " << baseNum << endl;
+	}
+	~Base()
+	{
+		cout << "~Base() : " << baseNum << endl;
+	}
+};
+
+class Derived : public Base
+{
+private:
+	int derivNum;
+public:
+	Derived(int n) : Base(n), derivNum(n)
+	{
+		cout << "Derived() : " << derivNum << endl;
+	}
+	~Derived()
+	{
+		cout << "~Derived() : " << derivNum << endl;
+	}
+};
+
+int main() {
+	Derived drv1(15);
+	Derived drv2(27);
+	return 0;
+
+}
