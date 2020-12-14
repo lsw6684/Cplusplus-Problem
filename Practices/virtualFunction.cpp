84053ac6af81082d70b1e47f198c733bf363f847
lsw6684 <lsw6684@hanmail.net>
2020-12-15 04:23:11 +0900

    This is an example of virtual function

---
+++ Practices/virtualFunction.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	virtual void eat()
+	{
+		cout << "I eat like a generic Animal.\n";
+	}
+};
+
+class Wolf : public Animal {
+public:
+	void eat() { cout << "I eat like a wolf\n"; }
+};
+
+class Fish : public Animal {
+public:
+	void eat() { cout << "I eat like a fish\n"; }
+};
+
+class OtherAnimal : public Animal {
+
+};
+
+int main()
+{
+	Animal* anAnimal[4];
+	anAnimal[0] = new Animal();
+	anAnimal[1] = new Wolf();
+	anAnimal[2] = new Fish();
+	anAnimal[3] = new OtherAnimal();
+	for (int i = 0; i < 4; i++)
+		anAnimal[i]->eat();
+	system("pause");
+	return 0;
+}
