9f687938302ffee15b12917b79bf883aebe295d6
lsw6684 <lsw6684@hanmail.net>
2020-12-17 23:12:22 +0900

    simple getline

---
+++ Practices/getline.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name, address, age;
+
+	cout << "이름은?";
+	getline(cin, name);
+
+	cout << "주소는?";
+	getline(cin, address);
+
+	cout << "나이는?";
+	getline(cin, age);
+
+	cout << name << ", " << address << ", " << age << "세\n";
+	return 0;
+}
