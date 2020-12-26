0cc34bf1126ed113cf129a6152020850435ea851
lsw6684 <lsw6684@hanmail.net>
2020-12-26 17:13:50 +0900

    Code Up 26

---
+++ CodeUp/start.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin >> a;
+	cin.ignore(256, ':');
+	cin >> b;
+	cin.ignore(256, ':');
+	cin >> c;
+	cout << b;
+
+	system("pause");
+	return 0;
+}
