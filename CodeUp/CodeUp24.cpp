a0ba07544167c3b0fc195da4d43a36eb0e66254c
lsw6684 <lsw6684@hanmail.net>
2020-12-26 17:06:05 +0900

    Code Up 24

---
+++ CodeUp/CodeUp24.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a[21];
+	cin >> a;
+	for (int i = 0; a[i] != 0; i++)
+	{
+		cout << "'" << a[i] << "'" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
