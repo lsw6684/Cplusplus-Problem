29ea5d071c318580df9e46d2be1ef796396901e4
lsw6684 <lsw6684@hanmail.net>
2020-12-26 17:40:42 +0900

    Code Up 38

---
+++ CodeUp/CodeUp38.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void aa(long long a, long long b)
+{
+	cout<< (a + b);
+}
+int main() {
+	long long a, b;
+	cin >> a >> b;
+	aa(a, b);
+	system("pause");
+	return 0;
+} 
