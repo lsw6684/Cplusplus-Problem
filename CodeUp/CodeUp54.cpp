07f50ee238749b33673bc2e429b3e1cc5570f00b
lsw6684 <lsw6684@hanmail.net>
2020-12-27 08:27:26 +0900

    Code Up 54

---
+++ CodeUp/CodeUp54.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a>>b;
+	if (a ==true && b==true)
+		cout << true;
+	else
+		cout << false;
+	system("pause");
+	return 0;
+}
