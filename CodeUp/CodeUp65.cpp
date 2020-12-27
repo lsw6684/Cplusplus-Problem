8fe13703e63cff974e82c517e726878177f510a3
lsw6684 <lsw6684@hanmail.net>
2020-12-27 10:21:45 +0900

    Code Up 65~66

---
+++ CodeUp/CodeUp65.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	cin >> a >>b>>c;
+	if (a % 2 == 0)
+		cout << a << endl;
+	if (b % 2 == 0)
+		cout << b << endl;
+	if (c % 2 == 0)
+		cout << c << endl;
+
+	system("pause");
+	return 0;
+}
