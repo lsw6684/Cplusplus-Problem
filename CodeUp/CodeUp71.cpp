e98ca467d80c968242d1d3c8fd0ae43ecbd26f4e
lsw6684 <lsw6684@hanmail.net>
2020-12-27 11:04:38 +0900

    Code Up 71

---
+++ CodeUp/CodeUp71.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int a;
+	while (true) {
+		cin >> a;
+		if (a == 0)
+		{
+			break;
+		}
+
+		cout << a << endl;
+	}
+
+	system("pause");
+	return 0;
+}
