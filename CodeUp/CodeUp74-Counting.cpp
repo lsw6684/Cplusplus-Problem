5a2b6978cc932192f1ee660bfd4a947fd86d8d99
lsw6684 <lsw6684@hanmail.net>
2020-12-27 16:22:12 +0900

    Code Up 74 - Counting

---
+++ CodeUp/CodeUp74-Counting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	while (true)
+	{
+
+		cout << a << endl;
+		if (a == 1)
+			break;
+		a--;
+	}
+
+
+	system("pause");
+	return 0;
+}
