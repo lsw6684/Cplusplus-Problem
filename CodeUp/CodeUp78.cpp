1c1c743744535722c56f7d7fe5a1ba95da922f96
lsw6684 <lsw6684@hanmail.net>
2020-12-27 16:35:59 +0900

    Code Up 78

---
+++ CodeUp/CodeUp78.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, sum=0;
+	cin >> a;
+	for (int i = 1; i <= a; i++)
+	{
+		if (i % 2 == 0)
+			sum += i;
+	}
+	cout << sum;
+
+	system("pause");
+	return 0;
+}
