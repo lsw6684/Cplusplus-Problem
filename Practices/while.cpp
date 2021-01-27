6a782b4bc21ae5769e8ad9778408e2ccbc227283
lsw6684 <lsw6684@hanmail.net>
2021-01-27 17:33:28 +0900

    just simple while

---
+++ Practices/while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, a, b, sum=0;
+	cout << "두 개의 정수 입력>>";
+	cin >> a >> b;
+
+	i=a;
+	while(i<=b) { // i가 b보다 작거나 같은 동안 반복
+		sum += i;
+		i++;
+	}
+	cout << a << "에서 " << b << "까지 합은 " << sum;
+}
